cea4afe2e53d3c167439ede0b7669f1977cba589
AlessandroToschi <ialessandrotoschi@gmail.com>
2015-10-17 18:49:50 +0200

    Initial Commit

---
+++ Tombola/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tombola
+//
+//  Created by Alessandro on 17/10/15.
+//  Copyright © 2015 Alessandro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tombola/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tombola
+//
+//  Created by Alessandro on 17/10/15.
+//  Copyright © 2015 Alessandro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
